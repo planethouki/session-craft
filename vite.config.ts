124e02272f27908a1651b53e87a7a348d0fbf340
planethouki <26595148+planethouki@users.noreply.github.com>
2025-12-14 17:05:03 +0900

    localhost https

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'node:fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('certs/localhost-key.pem'),
+      cert: fs.readFileSync('certs/localhost-cert.pem'),
+    },
+    host: 'localhost',
+    port: 5173,
+  },
 })
