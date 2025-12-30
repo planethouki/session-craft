d84527859e95f2e4135d5f5a869a02aec6285f0d
planethouki <26595148+planethouki@users.noreply.github.com>
2025-12-30 18:08:27 +0900

    pagesフォルダ

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { collection, getDocs, limit, orderBy, query } from 'firebase/firestore'
-import { db } from '../firebase'
-import type { Session } from '../types'
+import { db } from '../firebase.ts'
+import type { Session } from '../types.ts'
 import { Button, Container, Typography } from '@mui/material'
 import { Link } from 'react-router'
 
