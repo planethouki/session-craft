68d612150799441d54c8bfc7ef6a24fe448a5ede
planethouki <26595148+planethouki@users.noreply.github.com>
2026-02-14 15:43:38 +0900

    会話履歴をfirestoreに保存

--- functions/src/bot.ts
+++ functions/src/bot.ts
@@ -1,9 +1,10 @@
 import { onRequest } from 'firebase-functions/v2/https'
 import { defineSecret } from "firebase-functions/params";
 import * as logger from 'firebase-functions/logger'
+import * as admin from 'firebase-admin'
 
 import { googleAI } from '@genkit-ai/google-genai';
-import { genkit } from 'genkit';
+import { genkit, z } from 'genkit';
 
 import { messagingApi, WebhookRequestBody } from '@line/bot-sdk';
 
@@ -14,6 +15,8 @@ export const lineWebhook = onRequest({
   secrets: [ GOOGLE_GENAI_API_KEY, LINE_CHANNEL_ACCESS_TOKEN ],
 }, async (req, res) => {
   logger.info('LINE Webhook received', { body: req.body })
+
+  const db = admin.firestore();
 
   const body = req.body as WebhookRequestBody;
 
@@ -27,27 +30,57 @@ export const lineWebhook = onRequest({
 
   const helloFlow = ai.defineFlow({
     name: 'helloFlow',
-  }, async (name) => {
-    // make a generation request
-    // const { text } = await ai.generate(`Hello Gemini, my name is ${name}`);
+    inputSchema: z.object({ prompt: z.string(), messages: z.array(z.any()) })
+  }, async ({prompt, messages}) => {
     const { text } = await ai.generate({
+      prompt,
+      messages,
       model: googleAI.model('gemini-2.5-flash'),
-      prompt: name,
     });
 
     return text;
   });
 
+  const conversationsRef = db.collection('conversations');
+
   for (const event of body.events) {
     if (event.type !== 'message' || event.message.type !== 'text') continue;
 
-    const text = event.message.text;
-    const response = await helloFlow(text);
-    const result = await client.replyMessage({
+    if (event.source.type !== 'user') continue;
+    if (!event.source.userId) continue;
+
+    const userId = event.source.userId;
+    const userInputText = event.message.text;
+
+    const conversationQuery = await conversationsRef.get()
+    const messages = conversationQuery
+      .docs
+      .map(doc => doc.data())
+      .sort((a, b) => a.createdAt.seconds - b.createdAt.seconds)
+      .map(data => data.message);
+
+    logger.info('messages', messages);
+
+    const aiOutputMessage = await helloFlow({ prompt: userInputText, messages });
+
+    const lineResponseResult = await client.replyMessage({
       replyToken: event.replyToken,
-      messages: [{ type: 'text', text: response }],
+      messages: [{ type: 'text', text: aiOutputMessage }],
     })
-    logger.info('result', result);
+
+    logger.info('lineResponseResult', lineResponseResult);
+
+    await conversationsRef.doc().set({
+      userId,
+      message: { role: 'user', content: [{ text: userInputText }] },
+      createdAt: admin.firestore.FieldValue.serverTimestamp(),
+    });
+
+    await conversationsRef.doc().set({
+      userId,
+      message: { role: 'model', content: [{ text: aiOutputMessage }] },
+      createdAt: admin.firestore.FieldValue.serverTimestamp(),
+    });
   }
 
   // LINEからの検証用リクエストなどに対応するため、200 OKを返す
