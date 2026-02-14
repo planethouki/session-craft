a2499355dc583cbeab28adbc15764123fd1715bd
planethouki <26595148+planethouki@users.noreply.github.com>
2026-02-14 09:43:54 +0900

    LINE Webhook用テストエンドポイント

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import { setGlobalOptions } from 'firebase-functions'
+import { onRequest } from 'firebase-functions/v2/https'
 import * as admin from 'firebase-admin'
+import * as logger from 'firebase-functions/logger'
 
 setGlobalOptions({
   maxInstances: 10,
@@ -7,3 +9,10 @@ setGlobalOptions({
 })
 
 admin.initializeApp()
+
+export const lineWebhook = onRequest((req, res) => {
+  logger.info('LINE Webhook received', { body: req.body })
+
+  // LINEからの検証用リクエストなどに対応するため、200 OKを返す
+  res.status(200).send('OK')
+})
