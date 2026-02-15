24a25111f1c22c7888e17ad8c877bd88382fa935
planethouki <26595148+planethouki@users.noreply.github.com>
2026-02-15 15:35:44 +0900

    HTTPリクエストでスプレッドシートに書き込む処理をトリガー

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,6 +8,8 @@ import { WebhookRequestBody } from "@line/bot-sdk";
 
 import { handleEvent } from './services/botService'
 import { messageService } from "./services/messageService";
+import { getCurrentSession } from "./services/firestoreService";
+import { updateSpreadsheetSubmissions } from "./services/spreadsheetService";
 
 setGlobalOptions({
   maxInstances: 10,
@@ -40,6 +42,31 @@ export const lineWebhook = onRequest({
   );
 
   res.status(200).send('OK')
+})
+
+export const updateSpreadsheet = onRequest(async (req, res) => {
+  logger.info('Update spreadsheet request received');
+
+  try {
+    const session = await getCurrentSession();
+    if (!session) {
+      res.status(404).send('Active session not found');
+      return;
+    }
+
+    if (!session.spreadsheetIds || session.spreadsheetIds.length === 0) {
+      res.status(400).send('Spreadsheet IDs not set for the current session');
+      return;
+    }
+
+    await updateSpreadsheetSubmissions(session.sessionId, session.spreadsheetIds);
+
+    logger.info('Spreadsheet update successful', { sessionId: session.sessionId });
+    res.status(200).send('OK');
+  } catch (error) {
+    logger.error('Error updating spreadsheet', error);
+    res.status(500).send('Internal Server Error');
+  }
 })
 
 export * from './services/botService'
